7b04cf861f00a0795910243c635c84234de61385
anotherduardo <eduardo.iftm@gmail.com>
2018-10-22 13:40:30 -0200

    Segundo Commit

--- src/main/java/br/edu/iftm/extensaoSTS/repositories/AtividadeRepository.java
+++ src/main/java/br/edu/iftm/extensaoSTS/repositories/AtividadeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import br.edu.iftm.extensaoSTS.domain.Atividade;
@@ -11,7 +12,16 @@ import br.edu.iftm.extensaoSTS.domain.Atividade;
 @Repository
 public interface AtividadeRepository extends JpaRepository<Atividade, Integer>{
 	
+	// Colocamos aqui somente o que queremos de diferenete
+	// JpaRepository já contém um DAO implementado.
+	
     //@Query("Select a from Atividade a where a.nome like %:nome%")
+	// Containing é convensão: Automaticamente faz um like na query.
     public List<Atividade> findByNomeContaining(@Param("nome") String nome);
+    
+    
+    /*public ResponseEntity<?> deleteAtividade();*/
+ 
+    
     
 }//fim[AtividadeRepository]
--- src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
+++ src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
@@ -1,18 +1,18 @@
 package br.edu.iftm.extensaoSTS.resources;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import br.edu.iftm.extensaoSTS.domain.Atividade;
 import br.edu.iftm.extensaoSTS.services.AtividadeService;
 
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 
 @RestController
 @RequestMapping(value="/atividades")
@@ -23,9 +23,21 @@ public class AtividadeResource {
 	
 	@RequestMapping(value="{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id){
-		Atividade atividade = service.buscar(id);
-		return ResponseEntity.ok().body(atividade);
+		try {
+			Atividade atividade = service.buscar(id);
+			return ResponseEntity.ok().body(atividade);
+		}
+		catch(NoSuchElementException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
+	
+	/*
+	@DeleteMapping(value="{id}")
+	public ResponseEntity<?> delete(@PathVariable Integer id){
+		service.
+	}
+	*/
 	
 	/*
 	@RequestMapping(method=RequestMethod.GET)
